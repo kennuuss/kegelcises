d8179604eebdded8a9c6b8044e35af2a2db54430
kennuuss <maribor23072006gmail.com>
2024-11-09 21:44:55 +0100

    added tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+		'./Components/*.{jsx}',
+		'./Containers/*.{jsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
-

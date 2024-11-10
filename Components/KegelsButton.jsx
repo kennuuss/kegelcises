f0ca958f2b43127940de9712f84e27afacb1dd53
kennuuss <maribor23072006gmail.com>
2024-11-10 20:48:50 +0100

    started backend

--- Components/KegelsButton.jsx
+++ Components/KegelsButton.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-function KegelsButton() {
-	return <button className='bg-[#9B69FF] mx-auto rounded-full w-[244px] h-[244px]' />
+function KegelsButton(props) {
+	return (
+		<button
+			onMouseUp={() => {
+       () => props.setCount(props.currentCount + 1)
+			}}
+			className='bg-[#9B69FF] mx-auto rounded-full w-[244px] h-[244px]'
+		/>
+	)
 }
 
 export default KegelsButton
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import H1 from '../Components/H1'
 import KegelsButton from './../Components/KegelsButton'
 import Countdown from './../Components/Countdown'
-import RepCount from './../Components/RepCount';
+import RepCount from './../Components/RepCount'
+import { useEffect, useState } from 'react'
 
 function App() {
+	const [currentCount, setCount] = useState(0)
+	useEffect(() => console.log(currentCount), [currentCount])
 
 	return (
 		<div className='bg-white flex flex-col justify-center align-middle gap-[10%] h-[100vh]'>
 			<RepCount></RepCount>
 			<H1>Зажми для старта!</H1>
-			<KegelsButton />
+			<KegelsButton
+				currentCount={currentCount}
+				setCount={setCount} />
 			<Countdown> </Countdown>
 		</div>
 	)
